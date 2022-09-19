eba1366e56e40e13e869f872e3714c07a913083e
Ilya Kolesnikov <zloy-gamer@yandex.ru>
2022-09-20 00:11:56 +0500

    Add subscribe method for User model

---
+++ backend/users/conf.py
@@ -0,0 +1,14 @@
+# имя поля ошибки при ошибочном запросе к методу "subscribe"
+name_of_error_field = 'errors'
+# словарь с описанием возможных ошибок при запросе к методу "subscribe"
+subscription_errors = {
+    'add_to_yourself': {
+        name_of_error_field: 'Нельзя оформить подписку на себя.'
+    },
+    'already_exists': {
+        name_of_error_field: 'Вы уже оформили подписку на этого пользователя.'
+    },
+    'not_signed': {
+        name_of_error_field: 'Вы не подписаны на этого пользователя.'
+    }
+}
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,22 +1,26 @@
 from djoser.views import UserViewSet
 
 from django.contrib.auth import get_user_model
-from rest_framework.permissions import AllowAny
+from django.shortcuts import get_object_or_404
+from rest_framework import permissions
+from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
+from .conf import subscription_errors
+from .models import Subscription
 from .serializers import CustomUserSerializer
 
 User = get_user_model()
 
 
 class CustomUserViewSet(UserViewSet):
-    http_method_names = ('get', 'post')
     serializer_class = CustomUserSerializer
+    http_method_names = ('get', 'post', 'delete')
 
     def get_permissions(self):
         if self.action in ('list', 'retrieve'):
-            return (AllowAny(),)
+            return (permissions.AllowAny(),)
         return super().get_permissions()
 
     def get_serializer_class(self):
@@ -26,7 +30,45 @@ class CustomUserViewSet(UserViewSet):
 
     @action(detail=False)
     def subscriptions(self, request):
-        current_user = self.request.user
+        current_user = self.get_instance()
         subscriptions = current_user.following.all()
         serializer = self.get_serializer(subscriptions, many=True)
         return Response(serializer.data)
+
+    @action(detail=True, methods=['post', 'delete'])
+    def subscribe(self, request, id=None):
+        user_from = self.get_instance()
+        user_to = get_object_or_404(User, id=id)
+
+        if request.method == 'POST':
+            if user_from == user_to:
+                return Response(
+                    data=subscription_errors.get('add_to_yourself'),
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            subscription, created = Subscription.objects.get_or_create(
+                user_from=user_from,
+                user_to=user_to
+            )
+            if created:
+                serializer = self.get_serializer(user_to)
+                return Response(
+                    serializer.data,
+                    status=status.HTTP_201_CREATED
+                )
+            return Response(
+                data=subscription_errors.get('already_exists'),
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        subscription = Subscription.objects.filter(
+            user_from=user_from,
+            user_to=user_to
+        ).first()
+        if subscription:
+            subscription.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(
+            data=subscription_errors.get('not_signed'),
+            status=status.HTTP_400_BAD_REQUEST
+        )
